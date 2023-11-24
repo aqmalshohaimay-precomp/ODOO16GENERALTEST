1f87d01d04c22c6edf3234466d9cf4a757e5da5c
Mhar0027 <91427643+Mhar0027@users.noreply.github.com>
2023-11-24 17:25:36 +0800

    Reupload manifest file

---
+++ exchange_rate_module/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Morimoto Exchange Rate Module',
+    "author": "Precomp",
+    'version': '16.0.0',
+    "website": "",
+    'category': 'Extra Tools',
+    "support": "server2u.net",
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+    'application': True,
+}
